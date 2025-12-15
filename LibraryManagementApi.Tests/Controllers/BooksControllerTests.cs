af241b5bee332049da47c42b2254d7adf9883376
pawan-pathak12 <214448034+pawan-pathak12@users.noreply.github.com>
2025-12-15 09:06:21 +0545

    Test failed with NotFound

---
+++ LibraryManagementApi.Tests/Controllers/BooksControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+
+namespace LibraryManagementApi.Tests.Controllers
+{
+    [TestClass]
+    public class BooksControllerTests
+    {
+        private readonly HttpClient _httpClient;
+
+        public BooksControllerTests()
+        {
+            var factory = new WebApplicationFactory<Program>();
+            _httpClient = factory.CreateClient();
+        }
+        [TestMethod]
+        public async Task GetBooks_ReturnOk()
+        {
+            //Act 
+            var response = await _httpClient.GetAsync("/api/books");
+
+            //Assert 
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
